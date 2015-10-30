a32495e52afdaa07a058bc4b20f9d57456ea0d6b
landy lee <liqian@qiniu.com>
2015-10-30 18:42:33 +0800

    clear

--- qiniutest_test.go
+++ qiniutest_test.go
@@ -1,48 +1,45 @@
 package main_test
 
 import (
-    "fmt"
+	"./biz/exec"
+	"fmt"
+	"github.com/jmcvetta/napping"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	 "github.com/jmcvetta/napping"
-     "./biz/exec"
 )
 
 var _ = Describe("test4", func() {
 
-     Context("=4", func() {
-        It("cd:should be 4", func() {
-            Expect(4).To(Equal(4))
-            val,_:=util.Decode(util.EncodeType_Base64, "MzMz")
-            fmt.Println(val)
+	Context("=4", func() {
+		It("cd:should be 4", func() {
+			Expect(4).To(Equal(4))
+			val, _ := util.Decode(util.EncodeType_Base64, "MzMz")
+			fmt.Println(val)
 
-            // val,_:=util.Qiniu_base64(true,false,"333")
-            // fmt.Println(val)
-        })
-     })
+			// val,_:=util.Qiniu_base64(true,false,"333")
+			// fmt.Println(val)
+		})
+	})
 
+	Context("3", func() {
 
-     Context("3", func() {
-
-        It("z1:should be 3", func() {
-            url := "http://7xiuga.com1.z0.glb.clouddn.com/upload.jpg?avinfo"
-			resp, _ := napping.Get(url,nil, nil, nil)
-            Expect(resp.Status()).To(Equal(200))
+		It("z1:should be 3", func() {
+			url := "http://7xiuga.com1.z0.glb.clouddn.com/upload.jpg?avinfo"
+			resp, _ := napping.Get(url, nil, nil, nil)
+			Expect(resp.Status()).To(Equal(200))
 			Expect(resp.HttpResponse().Header["Content-Type"][0]).To(Equal("image/jpeg"))
-        })
+		})
 
+		PIt("smoke:p1 should be 2", func() {
 
+			By("Finding 2")
 
-        PIt("smoke:p1 should be 2", func() {
+			Expect(2).To(Equal(2))
 
-        	By("Finding 2")
+			By("Finding 3")
 
-            Expect(2).To(Equal(2))
-
-            By("Finding 3")
-
-            Expect(3).To(Equal(3))
-        })
-     })
+			Expect(3).To(Equal(3))
+		})
+	})
 
 })
