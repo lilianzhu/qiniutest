df602773f6a686f858e75fd4e5521c52399aa881
CHANGJUN JI <jichangjun@qiniu.com>
2015-11-11 10:14:59 +0800

    format project structure

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
-# if [ "$QBOXROOT" = "" ]; then
-#   QBOXROOT=$(cd ../; pwd)
-#   export QBOXROOT
-# fi
+ if [ "$QBOXROOT" = "" ]; then
+   QBOXROOT=$(cd ../; pwd)
+   export QBOXROOT
+ fi
 
 export GOPATH=$GOPATH:$QBOXROOT/qiniutest:$QBOXROOT/qiniutest/src:$QBOXROOT/qiniutest/bin
 export PATH=$PATH:$QBOXROOT/qiniutest:$QBOXROOT/qiniutest/bin
