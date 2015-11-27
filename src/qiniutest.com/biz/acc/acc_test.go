009097a4eec07286b25c3efefa47d8987836f66e
lilian zhu <zhujiali@qiniu.com>
2015-11-27 10:07:32 +0800

    add global definition

--- src/qiniutest.com/biz/acc/acc_test.go
+++ src/qiniutest.com/biz/acc/acc_test.go
@@ -1,5 +1,9 @@
 package acc_test
 
+import (
+	. "qiniutest.com/lib/def"
+)
+
 var _ = Describe("Acc", func() {
 	It("获取用户acctoken成功，返回200", func() {
 		resp, acctoken := GetAccToken(Env("username"), Env("password"))
---
+++ src/qiniutest.com/lib/def/global.go
@@ -0,0 +1,12 @@
+package def
+
+import (
+	"qiniutest.com/biz/acc"
+	"qiniutest.com/configs"
+)
+
+// Declarations for configs
+var Env = configs.ENV
+
+//Declarations for acc
+var GetAccToken = acc.GetAccToken
